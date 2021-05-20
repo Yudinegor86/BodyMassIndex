30124438bdbe3e6875bee1a91f893f45911e5bda
Yudin Egor <yudin.egor26@gmail.com>
2021-05-20 08:49:33 -0400

    corrected

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService calc = new BmiService();
+        double weight = 86;
+        double height = 1.77;
         double bmi = calc.calculate(86, 1.77);
         System.out.println("Body mass index: " + bmi);
 
